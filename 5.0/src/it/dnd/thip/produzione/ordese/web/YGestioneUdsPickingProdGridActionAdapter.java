8292b026c09ed37d49bc000b2f385c8b11cb6983
AGSOF3 <103518714+AGSOF3@users.noreply.github.com>
2026-04-14 14:54:14 +0200

    BASE

---
+++ 5.0/src/it/dnd/thip/produzione/ordese/web/YGestioneUdsPickingProdGridActionAdapter.java
@@ -0,0 +1,157 @@
+package it.dnd.thip.produzione.ordese.web;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+
+import com.thera.thermfw.ad.ClassADCollection;
+import com.thera.thermfw.base.Trace;
+import com.thera.thermfw.common.ErrorMessage;
+import com.thera.thermfw.persist.ConnectionManager;
+import com.thera.thermfw.persist.PersistentObject;
+import com.thera.thermfw.web.ServletEnvironment;
+import com.thera.thermfw.web.WebToolBar;
+import com.thera.thermfw.web.WebToolBarButton;
+import com.thera.thermfw.web.servlet.GridActionAdapter;
+import com.thera.thermfw.web.servlet.NavigationMapServlet;
+
+import it.dnd.thip.produzione.ordese.YGestioneUdsPickingProd;
+import it.dnd.thip.tuttoimballo.stampanti.YInterfStampanti;
+
+/**
+ * <p></p>
+ *
+ * <p>
+ * Company: Softre Solutions<br>
+ * Author: Daniele Signoroni<br>
+ * Date: 10/06/2025
+ * </p>
+ */
+
+/*
+ * Revisions:
+ * Number   Date        Owner    Description
+ * 71994    10/06/2025  DSSOF3   Prima stesura
+ */
+
+public class YGestioneUdsPickingProdGridActionAdapter extends GridActionAdapter {
+
+	private static final long serialVersionUID = 1L;
+
+	protected static final String RES = "it.dnd.thip.produzione.ordese.resources.YGestioneUdsPickingProd";
+	public static final String ACCODA_UDS = "ACCODA_RIGHE";
+
+	public static final String STP_ETI_LOFTWARE = "STP_ETI_LOFTWARE";
+
+	@Override
+	public void modifyToolBar(WebToolBar toolBar) {
+		super.modifyToolBar(toolBar);
+
+		WebToolBarButton accodaRighe = new WebToolBarButton("AccodaUds",
+				"action_submit", "new", "no", RES, "AccodaUds",
+				"it/thera/thip/vendite/ordineVE/images/AccodaRigheEva.gif", ACCODA_UDS, "single", true);
+
+		ServletEnvironment se = toolBar.getServletEnvironment();
+		if(se.getRequest().getParameter("YMostraUdsAzioneGeneraUdsAutomaticamente") != null 
+				&& se.getRequest().getParameter("YMostraUdsAzioneGeneraUdsAutomaticamente").equals("Y")){
+			WebToolBarButton btnNew = (WebToolBarButton) toolBar.getButtons().get(0);
+
+			toolBar.getButtons().clear();
+
+			toolBar.addButton(btnNew);
+		}else {
+			accodaRighe = null;
+		}
+
+		if(accodaRighe != null)
+			toolBar.addButton(accodaRighe);
+
+		WebToolBarButton stampaEticLoftware = new WebToolBarButton("StampaLoftware",
+				"action_submit", "infoArea", "no", RES, "StampaLoftware",
+				"it/dnd/thip/produzione/ordese/img/tt_imb.jpeg", STP_ETI_LOFTWARE, "multiple", true);
+		toolBar.addButton(stampaEticLoftware);
+	}
+
+	@Override
+	protected void otherActions(ClassADCollection cadc, ServletEnvironment se) throws ServletException, IOException {
+		String azione = getStringParameter(se.getRequest(), ACTION);
+		if(azione.equals(ACCODA_UDS)) {
+			String url = getCurrentViewSelector().getCopyObjectURL(cadc, se, getClass().getName());
+			//url += "&IdCodiceRigaLista"+getStringParameter(se.getRequest(), "IdCodiceRigaLista");
+			//url += "&IdCodiceLista"+getStringParameter(se.getRequest(), "IdCodiceLista");
+			url += "&YAccodamentoUdsRaccoltaDati=Y";
+
+			se.sendRequest(getServletContext(), url, false);
+		}else if(azione.equals(STP_ETI_LOFTWARE)) {
+			stampaEtichettaLoftware(cadc,se);
+		}else {
+			super.otherActions(cadc, se);
+		}
+	}
+
+	@Override
+	protected void callShowGrid(ServletEnvironment se) throws ServletException, IOException {
+		String np = se.getRequest().getParameter(NEW_PAGE);
+		String cn = se.getRequest().getParameter(CLASS_NAME);
+		String sk = se.getRequest().getParameter(TH_SETTING_KEY);
+		String url = se.getServletPath() + "com.thera.thermfw.web.servlet.Execute?Page=" + np + "&ClassName=" + cn + "&SettingKey=" + sk;
+		String sg = se.getRequest().getParameter(TH_SHOW_GRID_NAME);
+		if (sg != null && !sg.equals(""))
+			url += "&ShowGridName=" + sg;
+		String navigationMapName = se.getRequest().getParameter(NavigationMapServlet.NAV_MAP_NAME_PARAM);
+		String navigationMapBoxName = se.getRequest().getParameter(NavigationMapServlet.NAV_MAP_BOX_NAME_PARAM);
+		String navigationMapUserKey = se.getRequest().getParameter(NavigationMapServlet.NAV_MAP_BOX_USER_KEY_PARAM);
+
+		if (navigationMapName != null){
+			url += "&" + NavigationMapServlet.NAV_MAP_NAME_PARAM + "=" + navigationMapName;
+		}
+		if (navigationMapBoxName != null){
+			url += "&" + NavigationMapServlet.NAV_MAP_BOX_NAME_PARAM + "=" + navigationMapBoxName;
+		}
+		if (navigationMapUserKey != null){
+			url += "&" + NavigationMapServlet.NAV_MAP_BOX_USER_KEY_PARAM + "=" + navigationMapUserKey;
+		}
+		String MostraUdsAzioneGeneraUdsAutomaticamente = se.getRequest().getParameter("YMostraUdsAzioneGeneraUdsAutomaticamente");
+		if(MostraUdsAzioneGeneraUdsAutomaticamente != null && MostraUdsAzioneGeneraUdsAutomaticamente.equals("Y")) {
+			String newUrl = "";
+			newUrl+="&YMostraUdsAzioneGeneraUdsAutomaticamente=Y";;
+			newUrl+="&YIdTipoUds="+se.getRequest().getParameter("YIdTipoUds");
+			newUrl+="&YNumeroRitorno="+se.getRequest().getParameter("YNumeroRitorno");
+			newUrl+="&YIdCodiceRigaLista="+se.getRequest().getParameter("YIdCodiceRigaLista");
+			newUrl+="&YIdCodiceLista="+se.getRequest().getParameter("YIdCodiceLista");
+			newUrl+="&YPezziPerUds="+se.getRequest().getParameter("YPezziPerUds");
+
+			url += newUrl;
+		}
+		se.sendRequest(getServletContext(), url , false);
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	protected void stampaEtichettaLoftware(ClassADCollection cadc, ServletEnvironment se) throws ServletException, IOException {
+		String[] keys = se.getRequest().getParameterValues(OBJECT_KEY);
+		List errorMessages = new ArrayList();
+		try {
+			for (int i = 0; i < keys.length; i++) {
+				YGestioneUdsPickingProd uds = YGestioneUdsPickingProd.elementWithKey(keys[i], PersistentObject.NO_LOCK);
+				if(uds != null) {
+					YInterfStampanti stp = uds.recordLoftwareCartoni();
+					if(stp != null) {
+						int rc = stp.save();
+						if(rc > 0){
+							ConnectionManager.commit();
+						}else {
+							ConnectionManager.rollback();
+							errorMessages.add(new ErrorMessage("BAS0000078","Impossibile stampare l'etichetta"));
+						}
+					}
+				}
+			}
+		}catch (Exception e) {
+			e.printStackTrace(Trace.excStream);
+		}
+		se.getRequest().setAttribute("ErrorMessages", errorMessages);
+		se.sendRequest(getServletContext(), "com/thera/thermfw/common/InfoAreaHandler.jsp", false);
+	}
+}
---
+++ 5.0/src/it/dnd/thip/tuttoimballo/api/TuttoImballoService.java
@@ -0,0 +1,182 @@
+package it.dnd.thip.tuttoimballo.api;
+
+import java.math.BigDecimal;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Vector;
+
+import javax.ws.rs.core.Response.Status;
+
+import org.json.JSONObject;
+
+import com.thera.thermfw.base.Trace;
+import com.thera.thermfw.common.ErrorMessage;
+import com.thera.thermfw.persist.CachedStatement;
+import com.thera.thermfw.persist.ConnectionManager;
+import com.thera.thermfw.persist.Database;
+import com.thera.thermfw.persist.Factory;
+import com.thera.thermfw.persist.KeyHelper;
+import com.thera.thermfw.persist.PersistentObject;
+import com.thera.thermfw.rs.errors.ErrorUtils;
+
+import it.dnd.thip.produzione.ordese.YGestioneUdsPickingProd;
+import it.dnd.thip.produzione.ordese.YGestioneUdsPickingProdTM;
+import it.dnd.thip.tuttoimballo.stampanti.YInterfStampanti;
+import it.thera.thip.base.azienda.Azienda;
+
+/**
+ * Servizio per gestione api per Tutto per l'imballo
+ *
+ * <p>
+ * Company: Softre Solutions<br>
+ * Author: Andrea Gatta<br>
+ * Date: 03/09/2025
+ * </p>
+ */
+
+/*
+ * Revisions:
+ * Number	Date		Owner	Description
+ * 72106    03/09/2025  AGSOF3  Prima stesura    
+ * 72435	13/04/2026	AGSOF3	In modalita ridotta non ho inizio rilevazione quindi la query per recuperare
+ * 								la uds da pesare si basa ora sulla bilancia inserita nell operatore
+ */
+
+public class TuttoImballoService {
+
+	private static final String STMT_RECUPERA_LAST_UDS = 
+			"SELECT "
+			+ "	U.* "
+			+ "FROM "
+			+ "	THIPPERS.YGESTIONE_UDS_PICKING_PROD U "
+			+ "INNER JOIN THIP.PSN_DATI_PRD_UT P ON "
+			+ "	P.ID_AZIENDA = U.ID_AZIENDA "
+			+ "AND LEFT(U.R_UTENTE_CRZ, CHARINDEX('_', U.R_UTENTE_CRZ + '_') - 1) = P.ID_UTENTE_LGN	 "//72435
+			+ "INNER JOIN THIPPERS.YDIPENDENTI Y ON "
+			+ "	P.ID_AZIENDA = Y.ID_AZIENDA "
+			+ "	AND P.OPERATORE_DEF = Y.ID_DIPENDENTE "
+			+ "INNER JOIN THIPPERS.YBILANCIA_TUTTO_IMBALLO B ON "
+			+ "	B.ID_AZIENDA = P.ID_AZIENDA "
+			+ "	AND B.ID_BILANCIA = Y.R_BILANCIA_TI "
+			+ "WHERE "
+			+ "	STATO_UDS = '"+YGestioneUdsPickingProd.PACKING_COMPLETATO+"' "
+			+ "	AND B.IP = ? AND U.ID_AZIENDA = ? "
+			//+ "ORDER BY U.TIMESTAMP_AGG ASC";
+			+ "ORDER BY U.TIMESTAMP_CRZ ASC";
+	public static CachedStatement cSelectLastUds = new CachedStatement(STMT_RECUPERA_LAST_UDS);
+
+	private static TuttoImballoService instance;
+
+	public static TuttoImballoService getInstance() {
+		if (instance == null) {
+			instance = (TuttoImballoService) Factory.createObject(TuttoImballoService.class);
+		}
+		return instance;
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public JSONObject riceviPeso(String ip, BigDecimal peso, int progressivo) {
+		JSONObject result = new JSONObject();
+		JSONObject response = new JSONObject();
+		Status status = Status.OK;
+		Collection<ErrorMessage> errors = new Vector<ErrorMessage>();
+		try {
+			YGestioneUdsPickingProd uds = recuperaUdsPickingProd(Azienda.getAziendaCorrente(), ip);
+			if(uds != null) {
+				uds.setPesoUds(peso);
+				uds.setStatoUds(YGestioneUdsPickingProd.PESO_RICEVUTO);
+
+				int rc = uds.save();
+				if(rc > 0) {
+					Vector udsTutte = uds.listaUdsSameCodeSameList();
+
+					//..In caso di UDS multiarticolo devo chiudere le altre senza peso
+					//..il peso l'ho messo sulla prima e poi lo portero' sulla UDS testata
+					for (Iterator iterator = udsTutte.iterator(); iterator.hasNext();) {
+						YGestioneUdsPickingProd udsMA = (YGestioneUdsPickingProd) iterator.next();
+
+						udsMA.setStatoUds(YGestioneUdsPickingProd.PESO_RICEVUTO);
+						udsMA.save();
+					}
+
+					udsTutte.add(uds);
+
+					//.Lancio stampa etichetta 2 (scrivo un record nella tabella di tutto x imballo)
+					YInterfStampanti interfStampanti = uds.recordLoftwareCartoni();
+
+					rc = interfStampanti.save();
+					if(rc > 0)
+						ConnectionManager.commit();
+					else
+						ConnectionManager.rollback();
+				}else {
+					errors.add(new ErrorMessage("BAS0000078","Impossibile salvare UDS, rc = "+rc));
+				}
+			}else {
+				status = Status.INTERNAL_SERVER_ERROR;
+				errors.add(new ErrorMessage("BAS0000089","Nessuna UDS trovata"));
+			}
+		}catch(Exception e) {
+			status = Status.INTERNAL_SERVER_ERROR;
+			ErrorMessage em = new ErrorMessage("BAS0000078",e.getMessage());
+			//response.put("errors", ErrorUtils.getInstance().toJSON(em));
+			errors.add(em);
+			e.printStackTrace(Trace.excStream);
+		}	
+		response.put("errors", ErrorUtils.getInstance().toJSON(errors));
+		result.put("status", status);
+		result.put("response", response);
+		return result;
+	}
+
+	public static char calcolaCheckDigit(String codice17) {
+		if (codice17 == null || codice17.length() != 17 || !codice17.matches("\\d{17}")) {
+			throw new IllegalArgumentException("Servono esattamente 17 cifre.");
+		}
+
+		int somma = 0;
+		for (int i = 0; i < 17; i++) {
+			int cifra = codice17.charAt(i) - '0';
+			int peso = (i % 2 == 0) ? 3 : 1; // 1ª,3ª,5ª... -> 3 ; 2ª,4ª... -> 1
+			somma += cifra * peso;
+		}
+
+		int resto = somma % 10;
+		int check = (10 - resto) % 10; // “quanto manca alla prossima decina”
+		return (char) ('0' + check);
+	}
+
+
+	public YGestioneUdsPickingProd recuperaUdsPickingProd(String idAzienda, String ip) throws SQLException {
+		YGestioneUdsPickingProd uds = null;
+		ResultSet rs = null;
+		try{
+			PreparedStatement ps = cSelectLastUds.getStatement();
+			Database db = ConnectionManager.getCurrentDatabase();
+			db.setString(ps, 1, ip);
+			db.setString(ps, 2, idAzienda);
+			rs = ps.executeQuery();
+			if (rs.next()){
+				uds = (YGestioneUdsPickingProd) YGestioneUdsPickingProd.elementWithKey(YGestioneUdsPickingProd.class, KeyHelper.buildObjectKey(new String[] {
+						rs.getString(YGestioneUdsPickingProdTM.ID_AZIENDA),
+						rs.getString(YGestioneUdsPickingProdTM.NUMERO_RITORNO),
+						rs.getString(YGestioneUdsPickingProdTM.R_UDS),
+						rs.getString(YGestioneUdsPickingProdTM.R_COD_LISTA),
+						rs.getString(YGestioneUdsPickingProdTM.R_COD_RIGA_LISTA),
+				}), PersistentObject.NO_LOCK);
+			}
+		}finally{
+			try{
+				if(rs != null)
+					rs.close();
+			}catch(SQLException e){
+				e.printStackTrace(Trace.excStream);
+			}
+		}
+		return uds;
+	}
+
+}
