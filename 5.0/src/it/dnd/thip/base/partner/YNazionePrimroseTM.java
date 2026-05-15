f77df9792e817a28320d76d6cdb8d77b73f1dcd4
AGSOF3 <103518714+AGSOF3@users.noreply.github.com>
2026-05-15 08:36:27 +0200

    nazioni

--- 5.0/src/it/dnd/thip/base/partner/YNazionePrimroseTM.java
+++ 5.0/src/it/dnd/thip/base/partner/YNazionePrimroseTM.java
@@ -1,104 +1,53 @@
-/*
- * @(#)YNazionePrimroseTM.java
- */
+package it.dnd.thip.base.partner;
+
+import java.sql.SQLException;
+
+import com.thera.thermfw.base.SystemParam;
+
+import it.thera.thip.base.partner.NazionePrimroseTM;
 
 /**
- * YNazionePrimroseTM
- *
- * <br></br><b>Copyright (C) : Thera SpA</b>
- * @author Wizard 14/05/2026 at 17:02:53
- */
+* <p>
+* Company: Softre Solutions<br>
+* Author: Andrea Gatta<br>
+* Date: 15/05/2026
+* </p>
+*/
+
 /*
- * Revisions:
- * Date          Owner      Description
- * 14/05/2026    Wizard     Codice generato da Wizard
- *
- */
-package it.dnd.thip.base.partner;
-import com.thera.thermfw.persist.*;
-import com.thera.thermfw.common.*;
-import java.sql.*;
-import it.thera.thip.base.partner.*;
-import com.thera.thermfw.base.*;
+* Revisions:
+* Number   Date        Owner    Description
+* 72485	   15/05/2026  AGSOF3   Prima stesura
+*/
 
 public class YNazionePrimroseTM extends NazionePrimroseTM {
 
-  
-  /**
-   * Attributo R_CONTINENTE
-   */
-  public static final String R_CONTINENTE = "R_CONTINENTE";
+	public static final String R_CONTINENTE = "R_CONTINENTE";
 
-  /**
-   * Attributo R_SUBCONTINENTE
-   */
-  public static final String R_SUBCONTINENTE = "R_SUBCONTINENTE";
+	public static final String R_SUBCONTINENTE = "R_SUBCONTINENTE";
 
-  /**
-   * Attributo ID_AZIENDA
-   */
-  public static final String ID_AZIENDA = "ID_AZIENDA";
+	public static final String ID_AZIENDA = "ID_AZIENDA";
 
-  /**
-   *  TABLE_NAME
-   */
-  public static final String TABLE_NAME_EXT = SystemParam.getSchema("THIPPERS") + "YBBT34PT";
+	public static final String TABLE_NAME_EXT = SystemParam.getSchema("THIPPERS") + "";
 
-  /**
-   *  CLASS_NAME
-   */
-  private static final String CLASS_NAME = it.dnd.thip.base.partner.YNazionePrimrose.class.getName();
+	private static final String CLASS_NAME = it.dnd.thip.base.partner.YNazionePrimrose.class.getName();
 
-  
-  /**
-   *  YNazionePrimroseTM
-   * @throws SQLException
-   */
-  /*
-   * Revisions:
-   * Date          Owner      Description
-   * 14/05/2026    CodeGen     Codice generato da CodeGenerator
-   *
-   */
-  public YNazionePrimroseTM() throws SQLException {
-    super();
-  }
+	public YNazionePrimroseTM() throws SQLException {
+		super();
+	}
 
-  /**
-   *  initialize
-   * @throws SQLException
-   */
-  /*
-   * Revisions:
-   * Date          Owner      Description
-   * 14/05/2026    CodeGen     Codice generato da CodeGenerator
-   *
-   */
-  protected void initialize() throws SQLException {
-    super.initialize();
-    setObjClassName(CLASS_NAME);
-  }
+	protected void initialize() throws SQLException {
+		super.initialize();
+		setObjClassName(CLASS_NAME);
+	}
 
-  /**
-   *  initializeRelation
-   * @throws SQLException
-   */
-  /*
-   * Revisions:
-   * Date          Owner      Description
-   * 14/05/2026    Wizard     Codice generato da Wizard
-   *
-   */
-  protected void initializeRelation() throws SQLException {
-    super.initializeRelation();
-    linkTable(TABLE_NAME_EXT);
-    addAttributeOnTable("RContinente", R_CONTINENTE, TABLE_NAME_EXT);
-    addAttributeOnTable("IdAzienda", ID_AZIENDA, TABLE_NAME_EXT);
-    addAttributeOnTable("RSubcontinente", R_SUBCONTINENTE, TABLE_NAME_EXT);
-    
-    
+	protected void initializeRelation() throws SQLException {
+		super.initializeRelation();
+		linkTable(TABLE_NAME_EXT);
+		addAttributeOnTable("RContinente", R_CONTINENTE, TABLE_NAME_EXT);
+		addAttributeOnTable("IdAzienda", ID_AZIENDA, TABLE_NAME_EXT);
+		addAttributeOnTable("RSubcontinente", R_SUBCONTINENTE, TABLE_NAME_EXT);
 
-  }
+	}
 
 }
-
